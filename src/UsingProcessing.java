78b4247609a2581d38319978a9b51f12e818636f
cchaechae <seo23c@mtholyoke.edu>
2017-10-02 15:54:08 -0400

    First commit

---
+++ src/UsingProcessing.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class UsingProcessing extends PApplet  {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    	PApplet.main(UsingProcessing.class.getName());
+    }
+    
+    public void settings(){
+    	size(300,300);
+    }
+
+    public void setup(){
+    	fill(120,50,240);
+    }
+
+    public void draw(){
+    	ellipse(width/2, height/2, second(), second());
+    }
+    
+    
+}
